02f16831efaa7f9cc30d3fd374318ef15b278538
luk3ya0 <oneTOinf@163.com>
2023-03-29 09:27:42 +0800

    parse classdata as classfile

---
+++ gopher/classfile/attr_markers.go
@@ -0,0 +1,13 @@
+package classfile
+
+type MarkerAttribute struct{}
+type DeprecatedAttribute struct {
+	MarkerAttribute
+}
+type SyntheticAttribute struct {
+	MarkerAttribute
+}
+
+func (self *MarkerAttribute) readInfo(reader *ClassReader) {
+	// read nothing
+}
